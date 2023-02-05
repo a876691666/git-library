57b9163bd1a7293f9bc29a9fe5f26f78e098ad4b
a876691666 <928200728@qq.com>
2023-02-05 23:42:43 +0800

    add diesel sqlite mod

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use diesel::sqlite::SqliteConnection;
+use diesel::prelude::*;
+
+fn establish_connection() -> SqliteConnection {
+    let database_url = "sqlite:///tmp/test.db";
+    SqliteConnection::establish(database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+#[derive(Queryable)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub hair_color: Option<String>,
+}
+
+table! {
+    users (id) {
+        id -> Integer,
+        name -> Text,
+        hair_color -> Nullable<Text>,
+    }
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let connection = &mut establish_connection();
+    let users = users::table.load::<User>(connection).expect("Error loading users");
+    let string = users.iter().map(|user| format!("{}: {}", user.id, user.name)).collect::<Vec<String>>().join("\n");
+    HttpResponse::Ok().body(string)
 }
 
 #[actix_web::main]
